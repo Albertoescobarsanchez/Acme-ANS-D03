2f7b5b17c64e8ba3d59dc90700c5425b4e27ad43
celiasuaco <celiasuarezcoronel@gmail.com>
2025-03-29 16:47:36 +0100

    student04-task008

---
+++ src/main/java/acme/features/assistanceAgent/claim/AssistanceAgentClaimCompletedListService.java
@@ -0,0 +1,50 @@
+
+package acme.features.assistanceAgent.claim;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.client.components.models.Dataset;
+import acme.client.services.AbstractGuiService;
+import acme.client.services.GuiService;
+import acme.entities.claim.Claim;
+import acme.realms.AssistanceAgent;
+
+@GuiService
+public class AssistanceAgentClaimCompletedListService extends AbstractGuiService<AssistanceAgent, Claim> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	private AssistanceAgentClaimRepository repository;
+
+	// AbstractService interface ----------------------------------------------
+
+
+	@Override
+	public void authorise() {
+		boolean isAgent = super.getRequest().getPrincipal().hasRealmOfType(AssistanceAgent.class);
+		super.getResponse().setAuthorised(isAgent);
+
+	}
+
+	@Override
+	public void load() {
+		Collection<Claim> claims;
+		int agentId;
+		agentId = super.getRequest().getPrincipal().getActiveRealm().getId();
+		claims = this.repository.findCompletedClaims(agentId);
+
+		super.getBuffer().addData(claims);
+	}
+
+	@Override
+	public void unbind(final Claim claim) {
+		Dataset dataset;
+
+		dataset = super.unbindObject(claim, "registrationMoment", "passengerEmail", "claimType", "indicator");
+		super.getResponse().addData(dataset);
+
+	}
+}
