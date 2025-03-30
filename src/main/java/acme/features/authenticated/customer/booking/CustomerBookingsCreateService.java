1a283fa8a6ff4bea9345b38a37c0786c42b4ed54
2004o <2004o@192.168.1.65>
2025-03-30 14:23:46 +0200

    student02-task008

---
+++ src/main/java/acme/features/authenticated/customer/booking/CustomerBookingsCreateService.java
@@ -0,0 +1,109 @@
+
+package acme.features.authenticated.customer.booking;
+
+import java.util.Collection;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.client.components.models.Dataset;
+import acme.client.components.views.SelectChoices;
+import acme.client.helpers.MomentHelper;
+import acme.client.helpers.PrincipalHelper;
+import acme.client.services.AbstractGuiService;
+import acme.client.services.GuiService;
+import acme.entities.booking.Booking;
+import acme.entities.booking.TravelClass;
+import acme.entities.flight.Flight;
+import acme.entities.flight.FlightRepository;
+import acme.realms.Customer;
+
+@GuiService
+public class CustomerBookingsCreateService extends AbstractGuiService<Customer, Booking> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	private CustomerBookingsRepository	repository;
+
+	@Autowired
+	private FlightRepository			flightRepository;
+
+	// AbstractService interface ----------------------------------------------
+
+
+	@Override
+	public void authorise() {
+		boolean status;
+		status = super.getRequest().getPrincipal().hasRealmOfType(Customer.class);
+		super.getResponse().setAuthorised(status);
+	}
+
+	@Override
+	public void load() {
+		Booking booking;
+		int customerId;
+		Customer customer;
+		Date today = MomentHelper.getCurrentMoment();
+
+		customerId = super.getRequest().getPrincipal().getActiveRealm().getId();
+		customer = this.repository.findCustomerById(customerId);
+
+		booking = new Booking();
+		booking.setCustomer(customer);
+		booking.setPurchaseMoment(today);
+		booking.setDraftMode(true);
+
+		super.getBuffer().addData(booking);
+	}
+
+	@Override
+	public void bind(final Booking booking) {
+		assert booking != null;
+		Flight flight;
+		int flightId;
+
+		flightId = super.getRequest().getData("flight", int.class);
+		flight = this.flightRepository.findFlightById(flightId);
+
+		super.bindObject(booking, "locatorCode", "purchaseMoment", "travelClass", "draftMode", "lastNibble", "price");
+		booking.setFlight(flight);
+
+	}
+
+	@Override
+	public void validate(final Booking booking) {
+		Booking b = this.repository.findBookingByLocatorCode(booking.getLocatorCode());
+		if (b != null)
+			super.state(false, "locatorCode", "acme.validation.confirmation.message.booking.locator-code");
+	}
+
+	@Override
+	public void perform(final Booking booking) {
+		this.repository.save(booking);
+	}
+
+	@Override
+	public void unbind(final Booking booking) {
+		Dataset dataset;
+		SelectChoices choices;
+		SelectChoices flightChoices;
+
+		Collection<Flight> flights = this.flightRepository.findAllFlights();
+		flightChoices = SelectChoices.from(flights, "descriptions", booking.getFlight());
+		choices = SelectChoices.from(TravelClass.class, booking.getTravelClass());
+
+		dataset = super.unbindObject(booking, "locatorCode", "purchaseMoment", "draftMode", "lastNibble", "price");
+		dataset.put("travelClass", choices);
+		dataset.put("flight", flightChoices.getSelected().getKey());
+		dataset.put("flights", flightChoices);
+
+		super.getResponse().addData(dataset);
+	}
+
+	@Override
+	public void onSuccess() {
+		if (super.getRequest().getMethod().equals("POST"))
+			PrincipalHelper.handleUpdate();
+	}
+}
---
+++ src/main/java/acme/features/authenticated/customer/bookingRecord/CustomerBookingRecordRepository.java
@@ -0,0 +1,16 @@
+
+package acme.features.authenticated.customer.bookingRecord;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import acme.client.repositories.AbstractRepository;
+import acme.entities.booking.BookingRecord;
+
+@Repository
+public interface CustomerBookingRecordRepository extends AbstractRepository {
+
+	@Query("select br from BookingRecord br where br.booking.id = :bookingId AND br.passenger.id = :passengerId")
+	BookingRecord findBookingRecordById(@Param("bookingId") Integer bookingId, @Param("passengerId") Integer passengerId);
+}
