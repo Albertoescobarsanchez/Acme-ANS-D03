29a42b78fc2c407987a535b88bd8611f49e8bce0
Olga Cantalejo <olga@gmail.com>
2025-04-02 11:29:38 +0200

    group-task011

---
+++ src/main/java/acme/features/administrator/airport/AdministratorAirportRepository.java
@@ -0,0 +1,21 @@
+
+package acme.features.administrator.airport;
+
+import java.util.Collection;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import acme.client.repositories.AbstractRepository;
+import acme.entities.airport.Airport;
+
+@Repository
+public interface AdministratorAirportRepository extends AbstractRepository {
+
+	@Query("SELECT a FROM Airport a")
+	Collection<Airport> findAllAirports();
+
+	@Query("SELECT a FROM Airport a WHERE a.id = :id")
+	Airport findAirportById(int id);
+
+}
---
+++ src/main/java/acme/features/administrator/airport/AdministratorAirportShowService.java
@@ -0,0 +1,52 @@
+
+package acme.features.administrator.airport;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.client.components.models.Dataset;
+import acme.client.components.principals.Administrator;
+import acme.client.components.views.SelectChoices;
+import acme.client.services.AbstractGuiService;
+import acme.client.services.GuiService;
+import acme.entities.airport.Airport;
+import acme.entities.airport.OperationalScope;
+
+@GuiService
+public class AdministratorAirportShowService extends AbstractGuiService<Administrator, Airport> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	private AdministratorAirportRepository repository;
+
+	// AbstractService interface ----------------------------------------------
+
+
+	@Override
+	public void authorise() {
+		boolean isAdministrator = super.getRequest().getPrincipal().hasRealmOfType(Administrator.class);
+		super.getResponse().setAuthorised(isAdministrator);
+	}
+
+	@Override
+	public void load() {
+		int id;
+		Airport airport;
+
+		id = super.getRequest().getData("id", int.class);
+		airport = this.repository.findAirportById(id);
+
+		super.getBuffer().addData(airport);
+	}
+
+	@Override
+	public void unbind(final Airport airport) {
+		SelectChoices choices = SelectChoices.from(OperationalScope.class, airport.getOperationalScope());
+		Dataset dataset;
+
+		dataset = super.unbindObject(null, "name", "iataCode", "operationalScope", "city", "country", "website", "email", "phoneNumber");
+		dataset.put("operationalScope", choices);
+
+		super.getResponse().addData(dataset);
+	}
+}
